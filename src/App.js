7a306eda5912c5ce05636220fc857b5caaa9406c
Your Name <rebecca.solomon446@gmail.com>
2023-11-03 12:29:55 +0000

    creates and renders search component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Search from './components/Search';
 function App() {
   return (
     <div className="App">
       <body>
         <h1>My Recipe App</h1>
+          <Search/>
       </body>
     </div>
   );
---
+++ src/components/Search.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Search() {
+    return (
+        <h1>Search for recipes!</h1>
+    )
+}
+export default Search;
