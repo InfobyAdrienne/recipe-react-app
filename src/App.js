971a57a3cc0aa9d884251b34b53d07545caab4a8
Adrienne Galloway <aasgalloway@gmail.com>
2023-12-15 16:41:00 +0000

    getting the recipe steps via a new api call

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
 import Search from './components/Search';
-import React  from 'react';
-
-
-
+import React from 'react';
+import Recipe from './components/Recipe';
+import { useState } from 'react';
 
 function App() {
+  const [selectedMealId, setSelectedMealId] = useState(null); // Create state variable
+
+  function handleMealClick(id) {
+    setSelectedMealId(id); // Update state when meal is clicked
+  }
+
   return (
-    <div className="App">
-      <body>
-        <h1>My Recipe App</h1>
-          <Search/>
-      </body>
+    <div className="container">
+      <Search onMealClick={handleMealClick} />
+      {selectedMealId && <Recipe id={selectedMealId} />}{" "}
+      {/* Render Recipe component when selectedMealId is not null */}
     </div>
   );
 }
